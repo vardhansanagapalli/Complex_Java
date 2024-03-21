7c1e52492935ef4f1dc5bd6ea03631adff4a5661
vardhansanagapalli <vedasanagapalli@gmail.com>
2024-03-21 23:06:53 +0530

    Java Program to calculate BMI Program.

--- swappingwithoutvariables.java
+++ swappingwithoutvariables.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 
 public class swappingwithoutvariables {
 
-    public static void main(String[] args)     int a, b;
-        Scanne r sc = new Scanner(System.in);
-        System.out . println("Enter value of a: ");
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of a: ");
         a = sc.nextInt();
-        S y stem.out.println("Enter value of b: ");
+        System.out.println("Enter value of b: ");
         b = sc.nextInt();
-        S y stem.out.println("Value of a before swapping is " + a);
-        System.out.println("Value of a before swapping is "   + b);
-        a = a - b;  
-        b   =   b + a;
-        a   =   b - a;
-        S y s t em.out.println("Value of a after swapping is " + a);
-        System.out.println("Value of b after swapping is "   + b);
-    }  
+        System.out.println("Value of a before swapping is " + a);
+        System.out.println("Value of a before swapping is " + b);
+        a = a - b;
+        b = b + a;
+        a = b - a;
+        System.out.println("Value of a after swapping is " + a);
+     
+    }
 }
+
+//Code Author Vardhan Sanagapalli
